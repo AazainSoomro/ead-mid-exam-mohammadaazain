b9c614c064e021c1152dff10926db3dbafff78f6
Aazain <aazain.soomro@gmail.com>
2023-10-17 09:56:27 +0500

    initial table setup with static data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Table from "./Components/Table";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="bg-teal-950 text-teal-100 min-h-screen w-full">
+      <Table />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import sampleData from "../Data";
 
 function Table() {
-  return <div></div>;
+  const [Data, setData] = useState(sampleData);
+  return (
+    <div>
+      <div className="grid grid-cols-4 font-bold font-3xl uppercase">
+        <p>name</p>
+        <p>age</p>
+        <p>city</p>
+        <p>occupation</p>
+        <br />
+      </div>
+      {Data.map((user) => (
+        <div className="grid grid-cols-4">
+          <p>{user.name}</p>
+          <p>{user.age}</p>
+          <p>{user.city}</p>
+          <p>{user.occupation}</p>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Table;
